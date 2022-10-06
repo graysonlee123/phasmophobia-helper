bcf49338f16dd5c4e97e595c8d360c85ca96a0a7
Grayson Gantek <graysonleegantek@gmail.com>
2022-10-06 13:10:57 -0400

    updates head meta and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
 import Layout from '@components/layout'
+import Head from 'next/head'
 
 export default function Home() {
-  return <Layout />
+  return (
+    <>
+      <Head>
+        <title>Phasmophobia Helper</title>
+        <meta
+          name="description"
+          content="A web app that helps you narrow down your ghost type in the video game Phasmophobia."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Grayson Gantek" />
+        <meta name="color-scheme" content="dark" />
+      </Head>
+      <Layout />
+    </>
+  )
 }
