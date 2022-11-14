893c40e8ba157ea7da76b73de6589f8fe8469683
Grayson Gantek <graysonleegantek@gmail.com>
2022-11-14 11:56:27 -0500

    adds a markdown option to writing component

--- src/components/Writing/Writing.tsx
+++ src/components/Writing/Writing.tsx
@@ -1,10 +1,20 @@
 import { ReactNode } from 'react'
+import ReactMarkdown from 'react-markdown'
 import styles from './Writing.module.css'
 
 interface WritingProps {
   children: ReactNode
+  markdown?: boolean
 }
 
-export default function Writing({ children }: WritingProps) {
-  return <div className={styles.writing}>{children}</div>
+export default function Writing({ children, markdown = false }: WritingProps) {
+  return (
+    <div className={styles.writing}>
+      {typeof children === 'string' && markdown ? (
+        <ReactMarkdown linkTarget="_blank">{children}</ReactMarkdown>
+      ) : (
+        children
+      )}
+    </div>
+  )
 }
