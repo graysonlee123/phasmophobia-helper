3af035dd20c43391d9e01c708e026285d5ce3ef2
Grayson Gantek <graysonleegantek@gmail.com>
2022-11-14 18:02:20 -0500

    adds animation to checkboxes

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,7 +2,13 @@ import { MouseEvent } from 'react'
 import CheckboxIcon from '@components/CheckboxIcon'
 import Hint from '@components/Hint'
 import styles from './Checkbox.module.css'
+import { motion } from 'framer-motion'
 import cn from 'classnames'
+
+const variants = {
+  default: { opacity: 1 },
+  disabled: { opacity: 0.6 },
+}
 
 interface CheckboxProps {
   state: CheckboxState
@@ -14,12 +20,18 @@ interface CheckboxProps {
 
 export default function Checkbox({ state, primary, secondary, disabled, onClick }: CheckboxProps) {
   return (
-    <button onClick={onClick} className={cn(['button-reset', styles.button])} disabled={disabled}>
+    <motion.button
+      className={cn(['button-reset', styles.button])}
+      disabled={disabled}
+      variants={variants}
+      animate={disabled ? 'disabled' : 'default'}
+      onClick={onClick}
+    >
       <CheckboxIcon state={state} disabled={disabled} />
       <div className={styles.text}>
         <span className={styles.label}>{primary ?? 'Checkbox'}</span>
         {secondary && <Hint>{secondary}</Hint>}
       </div>
-    </button>
+    </motion.button>
   )
 }
