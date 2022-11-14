65181ab897beddad4af1e67a2defeb0e6924f5d2
Grayson Gantek <graysonleegantek@gmail.com>
2022-11-13 22:52:14 -0500

    active navbar link decoration

--- src/components/NavigationLinks/NavigationLinks.tsx
+++ src/components/NavigationLinks/NavigationLinks.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 import styles from './NavigationLinks.module.css'
 
 const links = [
@@ -22,17 +24,29 @@ const links = [
 ]
 
 export default function NavigationLinks() {
+  const { pathname } = useRouter()
+  const variants = {
+    active: { opacity: 1 },
+    inactive: { opacity: 0.6 },
+  }
+
   return (
     <ul className={styles.list}>
-      {links.map(({ href, label }) => (
-        <Fragment key={href}>
-          <li className={styles.item}>
-            <Link href={href}>
-              <a className={styles.anchor}>{label}</a>
-            </Link>
-          </li>
-        </Fragment>
-      ))}
+      {links.map(({ href, label }) => {
+        const isActive = pathname === href
+
+        return (
+          <Fragment key={href}>
+            <li className={styles.item}>
+              <motion.span variants={variants} animate={isActive ? 'active' : 'inactive'}>
+                <Link href={href}>
+                  <a className={styles.anchor}>{label}</a>
+                </Link>
+              </motion.span>
+            </li>
+          </Fragment>
+        )
+      })}
     </ul>
   )
 }
