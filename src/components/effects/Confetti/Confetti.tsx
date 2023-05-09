b5fba9eb9f1d1ca1fc08d39176d250e02830d4d2
Grayson Gantek <graysonleegantek@gmail.com>
2023-05-09 16:50:41 -0400

    confetti now covers the screen properly on scrolling and resizing

--- src/components/effects/Confetti/Confetti.tsx
+++ src/components/effects/Confetti/Confetti.tsx
@@ -2,6 +2,7 @@ import usePreferences from '@hooks/usePreferences'
 import useWinner from '@hooks/useWinner'
 import ReactConfetti from 'react-confetti'
 import { useWindowSize } from 'react-use'
+import styles from './Confetti.module.css'
 
 export default function Confetti() {
   const winner = useWinner()
@@ -11,12 +12,16 @@ export default function Confetti() {
   if (!winner || preferences.confetti === false) return null
 
   return (
-    <ReactConfetti
-      width={width}
-      height={height}
-      numberOfPieces={400}
-      gravity={0.2}
-      recycle={false}
-    />
+    <div className={styles.wrapper}>
+      <ReactConfetti
+        width={width}
+        height={height}
+        numberOfPieces={400}
+        gravity={0.2}
+        initialVelocityY={5}
+        recycle={false}
+        className={styles.canvas}
+      />
+    </div>
   )
 }
